a3c6613157e472bbb0f6bdfb87937d2579e5324b
Winnie0211 <93068530+Winnie0211@users.noreply.github.com>
2022-04-17 14:25:21 +0800

    output 101 ~ 150

--- main.cpp
+++ main.cpp
@@ -11,11 +11,15 @@ int main(){
 	//
 	cout<<"KK:"<<"asdfjk;"<<endl;
 	//function output 51~100
-	'
+	
 	//
 	cout<<"WT:"<<"Hello:)"<<endl;
 	//function output 101~150
-	
+	int n = 100;
+	for(int i; i < 51; i ++){
+		n = n + 1;
+		cout<<n<<endl;
+	}
 	//
 	system("pause");
 }
