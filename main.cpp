07db76939f0fd5e47b5def90f045f79c41edbf42
kayneyao <42885964+kayneyao@users.noreply.github.com>
2022-04-17 13:50:38 +0800

    Speak

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(){
 	cout<<"hello world!"<<endl;
 	cout<<"THE MAIN"<<endl;
 	
-	cout<<"MB:"<<""<<endl;
+	cout<<"MB:"<<"asd;lfjksfd"<<endl;
 	cout<<"KK:"<<""<<endl;
 	cout<<"WT:"<<""<<endl;
 	
