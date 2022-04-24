a39a5f523ac4837a0abe71368a84ecba84f92276
kirkkao <kirkkao2010@gmail.com>
2022-04-24 14:03:38 +0800

    classwork2 done

---
+++ Number_Printer.h
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int print_Numbers(int num,int num2){
+	int i;
+	for(i=num;i<=num2;i++){
+		cout<<i<<endl;
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
-
+#include "Number_Printer.h"
 #include "SayHi.h"
 
 using namespace std;
 int main(){
-	SayHallo();
-	SayNameHallo("Kirk Kao");
-	SayNameHalloWithIf("");
+	
 	cout<<"THE MAIN"<<endl;
 	
 	cout<<"MB:"<<"asd;lfjksfd"<<endl;
@@ -15,7 +13,10 @@ int main(){
 	//
 	cout<<"KK:"<<"Kirk Kao"<<endl;
 	//function output 51~100
-	   
+		print_Numbers(100,222);
+		SayHallo();
+		SayNameHallo("Kirk Kao");
+		SayNameHalloWithIf("");
 	//
 	cout<<"WT:"<<"Hello:)"<<endl;
 	//function output 1~100 ªº½è¼Æ 
