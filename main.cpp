aa94e0f92dc225f8d8fb64064baf0799f4b3e644
kirkkao <kirkkao2010@gmail.com>
2022-04-24 15:38:30 +0800

    Done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<string>
 #include"sayHi.h"
-
 #include"Number_printer.h"
-
+#include"mathFunctions.h"
+#include"primeNumber.h"
 using namespace std;
 int main(){
 	
@@ -15,15 +15,21 @@ int main(){
 	
 	cout<<"MB:"<<"asd;lfjksfd"<<endl;
 	//function 99乘法表 
-	
+		prime(25);
+		fibonacchiSq(0,1,20);
+		halfPerfectNum(10);
+		timeTables();
 	//
 	cout<<"KK:"<<"asdfjk;"<<endl;
 	//function output 51~100
-	 print_Numbers(1,20);
+		print_Numbers(1,20);
+		SayHello();
+		SayNameHello("KK");
+		SayNameHelloWithIf("");
 	//
 	cout<<"WT:"<<"Hello:)"<<endl;
 	//function output 1~100 的質數 
-	
+		primeNumber();
 	//
 	system("pause");
 }
